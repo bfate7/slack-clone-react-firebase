a4cbc0f2170dc47c6dedf2234c97c11194836fb4
fateh <f.boug93@gmail.com>
2020-10-28 12:48:33 +0100

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Main from "./components/Main";
 
 function App() {
-  return <h1>Slack Clone</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/register" component={Register} exact />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
