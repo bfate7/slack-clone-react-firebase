f13558fff5b149078b0541968f7e18da990d16df
fateh <f.boug93@gmail.com>
2020-10-30 16:19:06 +0100

    add clear user action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -3,3 +3,7 @@ import * as actionTypes from "./types";
 export const setUser = (user) => {
   return { type: actionTypes.SET_USER, payload: user };
 };
+
+export const clearUser = (user) => {
+  return { type: actionTypes.CLEAR_USER };
+};
