dc0837a0b8b8164054f954c571215b24e27df3f7
fateh <f.boug93@gmail.com>
2020-10-30 12:11:46 +0100

    add redirect on auth state change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import "./App.css";
-import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Main from "./components/Main";
+import { useEffect } from "react";
+import firebase from "./firebase";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        props.history.push("/");
+      }
+    });
+  }, [props.history]);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/login" component={Login} exact />
-        <Route path="/register" component={Register} exact />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" component={Main} exact />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
   );
 }
 
-export default App;
+const AppWithRouter = withRouter(App);
+
+const AppWithAuth = () => (
+  <Router>
+    <AppWithRouter />
+  </Router>
+);
+
+export default AppWithAuth;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+
+//css
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
