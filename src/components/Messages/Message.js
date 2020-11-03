367aea97bf8bd102fca0472e39c60f0022c629ac
fateh <f.boug93@gmail.com>
2020-11-03 11:30:53 +0100

    updateMessage

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Comment } from "semantic-ui-react";
+import { Comment, Image } from "semantic-ui-react";
+import moment from "moment";
 
 const Message = ({ user, message }) => {
   const isOwnMessage = (message, user) =>
     message.user.id === user.uid ? "message__self" : "";
+
+  const isImage = (message) =>
+    message.hasOwnProperty("image") && !message.hasOwnProperty("content");
 
   return (
     <Comment>
       <Comment.Avatar src={message.user.avatar} />
       <Comment.Content className={isOwnMessage(message, user)}>
         <Comment.Author as="a">{message.user.name}</Comment.Author>
-        <Comment.Metadata>{message.timestamp}</Comment.Metadata>
-        <Comment.Text>{message.content}</Comment.Text>
+        <Comment.Metadata>
+          {moment(message.timestamp).fromNow()}
+        </Comment.Metadata>
+
+        {isImage(message) ? (
+          <Image src={message.image} className="message__image" />
+        ) : (
+          <Comment.Text>{message.content}</Comment.Text>
+        )}
       </Comment.Content>
     </Comment>
   );
