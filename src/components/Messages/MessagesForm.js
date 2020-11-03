83d38d6ab598469fcb5193cb74635759b96eca81
fateh <f.boug93@gmail.com>
2020-11-03 11:30:39 +0100

    update messagesForm

--- src/components/Messages/MessagesForm.js
+++ src/components/Messages/MessagesForm.js
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
 import firebase from "../../firebase";
 import { Button, Input, Segment } from "semantic-ui-react";
+import FileModal from "./FileModal";
+import { v4 as uuidv4 } from "uuid";
 
 const MessagesForm = (props) => {
   const messagesRef = firebase.database().ref("messages");
+  const storageRef = firebase.storage().ref();
+
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const createMessage = () => ({
-    timestamp: firebase.database.ServerValue.TIMESTAMP,
-    content: message,
-    user: {
-      id: props.currentUser.uid,
-      name: props.currentUser.displayName,
-      avatar: props.currentUser.photoURL,
-    },
-  });
+  const [modal, setModal] = useState(false);
+
+  //upload
+  const [uploadState, setUploadState] = useState(null);
+  const [percentUpload, setPercentUpload] = useState(0);
+
+  const createMessage = (fileURL = null) => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: props.currentUser.uid,
+        name: props.currentUser.displayName,
+        avatar: props.currentUser.photoURL,
+      },
+    };
+
+    if (fileURL) {
+      message["image"] = fileURL;
+    } else {
+      message["content"] = message;
+    }
+
+    return message;
+  };
 
   const sendMessage = (e) => {
     if (message) {
@@ -32,6 +51,50 @@ const MessagesForm = (props) => {
           console.log(err);
         });
     }
+  };
+
+  const sendFileMessage = (fileURL) => {
+    messagesRef
+      .child(props.currentChanel.id)
+      .push()
+      .set(createMessage(fileURL))
+      .then(() => {
+        setUploadState("done");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const uploadFile = (file, metadata) => {
+    //set uploading state
+    setUploadState("uploading");
+    //set filePath
+    const filePath = `chat/public/${uuidv4()}`;
+    //set uploadTask
+    const uploadTask = storageRef.child(filePath).put(file, metadata);
+
+    uploadTask.on(
+      "state_changed",
+      (snap) => {
+        const percentUpload = Math.round(
+          (snap.bytesTransferred / snap.totalBytes) * 100
+        );
+        console.log(percentUpload);
+
+        setPercentUpload(percentUpload);
+      },
+      (err) => {
+        alert("err uploading");
+      }
+    );
+
+    uploadTask.then(() => {
+      setUploadState("done");
+      uploadTask.snapshot.ref.getDownloadURL().then((fileURL) => {
+        sendFileMessage(fileURL);
+      });
+    });
   };
 
   return (
@@ -61,8 +124,14 @@ const MessagesForm = (props) => {
           content="Upload Media"
           labelPosition="right"
           icon="cloud upload"
+          onClick={() => setModal(true)}
         />
       </Button.Group>
+      <FileModal
+        modal={modal}
+        closeModal={() => setModal(false)}
+        uploadFile={uploadFile}
+      />
     </Segment>
   );
 };
