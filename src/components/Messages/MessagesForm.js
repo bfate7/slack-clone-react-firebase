a06210c09850327ac1836dfcada11b754b125a78
fateh <f.boug93@gmail.com>
2020-12-13 14:59:26 +0100

    add  emoji picker

--- src/components/Messages/MessagesForm.js
+++ src/components/Messages/MessagesForm.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import firebase from "../../firebase";
 import { Button, Input, Segment } from "semantic-ui-react";
 import FileModal from "./FileModal";
 import { v4 as uuidv4 } from "uuid";
 import ProgressBar from "./ProgressBar";
+
+//emoji picker
+import { Picker, emojiIndex } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 const storageRef = firebase.storage().ref();
 const typingRef = firebase.database().ref("typing");
@@ -12,10 +16,14 @@ const MessagesForm = (props) => {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [modal, setModal] = useState(false);
+  const messageInputRef = useRef(null);
 
   //upload
   const [uploadState, setUploadState] = useState(false);
   const [percentUpload, setPercentUpload] = useState(0);
+
+  //emojie picker state
+  const [emojiPicker, setEmojiePicker] = useState(false);
 
   const createMessage = (fileURL = null) => {
     const messagetoSend = {
@@ -121,8 +129,43 @@ const MessagesForm = (props) => {
     }
   };
 
+  const handleTogglePicker = () => {
+    setEmojiePicker(!emojiPicker);
+  };
+
+  const handleAddEmoji = (emoji) => {
+    const newMessage = colonToUnicode(`${message} ${emoji.colons}`);
+    setMessage(newMessage);
+    messageInputRef.current.focus();
+  };
+
+  const colonToUnicode = (message) => {
+    return message.replace(/:[A-Za-z0-9_+-]+:/g, (x) => {
+      x = x.replace(/:/g, "");
+      let emoji = emojiIndex.emojis[x];
+      if (typeof emoji !== "undefined") {
+        let unicode = emoji.native;
+        if (typeof unicode !== "undefined") {
+          return unicode;
+        }
+      }
+      x = ":" + x + ":";
+      return x;
+    });
+  };
+
   return (
     <Segment className="message__form">
+      {emojiPicker && (
+        <Picker
+          set="apple"
+          title="pick your emoji"
+          emoji="point_up"
+          style={{ marginBottom: "1em" }}
+          onSelect={handleAddEmoji}
+        />
+      )}
+
       <Input
         fluid={true}
         name="message"
@@ -130,10 +173,16 @@ const MessagesForm = (props) => {
         onKeyDown={handleKeyDown}
         value={message}
         style={{ marginBottom: "0.7em" }}
-        label={<Button icon={"add"} />}
+        label={
+          <Button
+            icon={!emojiPicker ? "add" : "remove"}
+            onClick={handleTogglePicker}
+          />
+        }
         labelPosition="left"
         placeholder="Write your message"
         disabled={loading}
+        ref={messageInputRef}
       />
       <Button.Group icon widths="2">
         <Button
